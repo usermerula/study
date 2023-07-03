ec0f28efd6509b7820269dc856054c49f7763afc
Xalik Zagirov <xxnope@mail.ru>
2023-07-03 14:50:44 +0300

    Example 001

--- EXAMPLE001/Program.cs
+++ EXAMPLE001/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+
+else
+{
+    Console.Write("Привет, ")
+    Console.WriteLine(username);
+}
