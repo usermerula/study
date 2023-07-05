daf86dcbc38642c042a53b95096800cdc45af00d
Xalik Zagirov <xxnope@mail.ru>
2023-07-06 00:07:05 +0300

    Третье дз

---
+++ EXAMPLE003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите  число");
+int nmbr = Convert.ToInt32(Console.ReadLine());
+
+if (nmbr % 2 == 0)
+{
+    Console.WriteLine("Число является четным!");
+}
+else
+{
+    Console.WriteLine("Число является нечетным!");
+}
