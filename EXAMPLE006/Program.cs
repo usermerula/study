a9fe4e1b123e9b35c1089de3ff6f754937b89fe2
usermerula <doublenope@mail.ru>
2023-07-09 00:35:29 +0300

    Задача 10

--- EXAMPLE006/Program.cs
+++ EXAMPLE006/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.Clear();
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100 || number > 1000)
+    Console.WriteLine("Вы ввели не трехзначное число!");
+else  
+    Console.WriteLine(number / 10 % 10);
