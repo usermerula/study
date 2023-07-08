dfaf2c7e58f74723cf822dd82a237fe954748df5
usermerula <doublenope@mail.ru>
2023-07-09 00:46:25 +0300

    Задача 13

--- EXAMPLE007/Program.cs
+++ EXAMPLE007/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100 || number > 1000)
+    Console.WriteLine("Вы ввели не трехзначное число");
+else
+    Console.WriteLine(number % 10);
