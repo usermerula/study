78f093a44e21c13091c06600e9b70b01650dec87
usermerula <doublenope@mail.ru>
2023-07-09 00:21:22 +0300

    Задача 15

---
+++ EXAMPLE005/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите день недели: 1-7");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number == 7 || number == 6)
+    Console.WriteLine("Выходной!");
+else
+    Console.WriteLine("Не выходной!");
