48d4d5fa96f487593233f3f3f571a086d6be3dde
usermerula <doublenope@mail.ru>
2023-07-15 00:33:00 +0300

    Задача 21

--- EXAMPLE006/Program.cs
+++ EXAMPLE006/Program.cs
@@ -1,9 +1,21 @@
-﻿Console.Clear();
+﻿Console.WriteLine("Введите координату X первой точки: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y первой точки: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Z первой точки: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату X второй точки: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y второй точки: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Z второй точки: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите трехзначное число");
-int number = Convert.ToInt32(Console.ReadLine());
+int A = x2 - x1;
+int B = y2 - y1;
+int C = z1 - z2;
 
-if (number < 100 || number > 1000)
-    Console.WriteLine("Вы ввели не трехзначное число!");
-else  
-    Console.WriteLine(number / 10 % 10);
+double length = Math.Sqrt(A * A + B * B + C * C);
+Console.WriteLine($"Длинна отрезка {length}");
+
+
