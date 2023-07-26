ac2a25df7fb3ba51f547a331bb8d9b4dbd300df6
usermerula <doublenope@mail.ru>
2023-07-26 12:12:28 +0300

    Задача 38

--- EXAMPLE006/Program.cs
+++ EXAMPLE006/Program.cs
@@ -1,21 +1,33 @@
-﻿Console.WriteLine("Введите координату X первой точки: ");
-int x1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координату Y первой точки: ");
-int y1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координату Z первой точки: ");
-int z1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координату X второй точки: ");
-int x2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координату Y второй точки: ");
-int y2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координату Z второй точки: ");
-int z2 = Convert.ToInt32(Console.ReadLine());
+﻿Console.Write("Введите количество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+double[] randomArray = new double[a];
 
-int A = x2 - x1;
-int B = y2 - y1;
-int C = z1 - z2;
+void mas(int a)
+{
+    Random rand = new Random();
+    for (int i = 0; i < a; i++)
+    {
+        randomArray[i] = rand.NextDouble();
+        Console.Write($"{ randomArray[i]:F2} ");
+}
 
-double length = Math.Sqrt(A * A + B * B + C * C);
-Console.WriteLine($"Длинна отрезка {length}");
+}
 
+double raz(double[] randomArray)
+{
+    double min = randomArray[0];
+    double max = randomArray[0];
+    int i = 1;
+    while (i < randomArray.Length)
+    {
+        if (max < randomArray[i])
+            max = randomArray[i];
+        if (min > randomArray[i])
+            min = randomArray[i];
+        i = i + 1;
+    }
+    return max - min;
+}
 
+mas(a);
+Console.Write($"\nРазница между максимальным и минимальным элементов массива: { raz(randomArray):F2}");
