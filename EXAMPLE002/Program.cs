556d4c7352b2c85526781586b950ce3c3b6428e6
usermerula <doublenope@mail.ru>
2023-07-26 11:51:52 +0300

    Задача 27

--- EXAMPLE002/Program.cs
+++ EXAMPLE002/Program.cs
@@ -1,14 +1,27 @@
-﻿Console.WriteLine("Введите первое число");
+﻿Console.Write("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
-int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите третье число");
-int c = Convert.ToInt32(Console.ReadLine());
+int len = Length(a);
+Console.Write($"Сумма цифр в числе { a} равно: { Sum(a, len)}");
 
-int max = a;
 
-if (b > max) max = b;
+int Length(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
 
-if (c > max) max = c;
-
-Console.WriteLine(max);
+int Sum(int a, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += a % 10;
+        a /= 10;
+    }
+    return sum;
+}
