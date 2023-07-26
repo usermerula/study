ab9e954d071f72a42af81f21fbb63e722396f8ab
usermerula <doublenope@mail.ru>
2023-07-26 12:05:02 +0300

    Задача 34

--- EXAMPLE004/Program.cs
+++ EXAMPLE004/Program.cs
@@ -1,9 +1,27 @@
-﻿Console.Clear();
-Console.WriteLine("Введите число");
-int n = Convert.ToInt32(Console.ReadLine());
+﻿Console.Write("Введите количество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] randomArray = new int[a];
 
-for (int i = 1; i <= n; i++)
+void mas(int a)
 {
-    if (i % 2 == 0)
-        System.Console.Write(i + " ");
+for (int i = 0; i < a; i++)
+{
+randomArray[i] = new Random().Next(99,999);
+Console.Write(randomArray[i] + " " );
 }
+
+}
+
+int kol(int[] randomArray)
+{
+int kol = 0;
+for (int i = 0; i < randomArray.Length; i++)
+{
+if (randomArray[i] % 2 == 0)
+kol = kol + 1;
+}
+return kol;
+}
+
+mas(a);
+Console.Write($"\nКоличество чётных чисел в массиве: {kol(randomArray)}");
