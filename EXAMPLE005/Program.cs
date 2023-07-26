70ac5d2ad3436e969cd69bf46a43cc7227aba633
usermerula <doublenope@mail.ru>
2023-07-26 12:08:30 +0300

    Задача 36

--- EXAMPLE005/Program.cs
+++ EXAMPLE005/Program.cs
@@ -1,19 +1,28 @@
-﻿Console.WriteLine("Введите число: ");
-string number = Console.ReadLine();
-int size = number.Length;
+﻿Console.Write("Введите количество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] randomArray = new int[a];
 
-if (size == 5)
+void mas(int a)
 {
-    if (number[0] == number[4] && number[1] == number[3])
-    {
-        Console.WriteLine($"{number} - Палиндром");
-    }
-    else
-    {
-        Console.WriteLine($"{number} - НЕ палиндром");
-    }
-}
-else
+for (int i = 0; i < a; i++)
 {
-    Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+randomArray[i] = new Random().Next(1,9);
+Console.Write(randomArray[i] + " ");
 }
+
+}
+
+int kol(int[] randomArray)
+{
+int sum = 0;
+int i = 0;
+while (i < randomArray.Length)
+{
+sum = sum + randomArray[i];
+i = i + 2;
+}
+return sum;
+}
+
+mas(a);
+Console.Write($"\nCумма элементов, стоящих на нечётных позициях: {kol(randomArray)}");
