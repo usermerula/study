07f903985519be9812474203559f1b9b01873450
usermerula <doublenope@mail.ru>
2023-07-26 11:49:25 +0300

    Задача 25

--- EXAMPLE001/Program.cs
+++ EXAMPLE001/Program.cs
@@ -1,15 +1,16 @@
-﻿Console.WriteLine("Введите первое число");
+﻿Console.Write("Введите число A: ");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
+Console.Write("Введите число B: ");
 int b = Convert.ToInt32(Console.ReadLine());
-
-if (a > b)
-{
-    Console.Write("Ответ:");
-    Console.WriteLine(a);
-}
+int c = 1;
+if (b < 1)
+    Console.Write("Число B должно быть натуральным\n");
 else
 {
-    Console.Write("Ответ:");
-    Console.WriteLine(b);
+    while (b != 0)
+    {
+        c = c * a;
+        b --;
+    }
+    Console.Write($"Число A в степени B равно { c}");
 }
