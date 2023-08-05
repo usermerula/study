de0476f8d5b433dffa0fe65ff69a81acac1f47de
usermerula <doublenope@mail.ru>
2023-08-06 01:09:26 +0300

    Задача 50

--- EXAMPLE002/Program.cs
+++ EXAMPLE002/Program.cs
@@ -1,27 +1,31 @@
-﻿Console.Write("Введите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-int len = Length(a);
-Console.Write($"Сумма цифр в числе { a} равно: { Sum(a, len)}");
+﻿Console.Write("Размер m массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Размер n массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] randomArray = new double[m,n];
+Console.Write("Позицию строки: ");
+int m2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Позицию столбца: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
 
-
-int Length(int a)
+void mas(int m, int n)
 {
-    int index = 0;
-    while (a > 0)
-    {
-        a /= 10;
-        index++;
-    }
-    return index;
-}
-
-int Sum(int a, int len)
+int i,j;
+Random rand = new Random();
+for (i = 0; i < m; i++)
 {
-    int sum = 0;
-    for (int i = 1; i <= len; i++)
-    {
-        sum += a % 10;
-        a /= 10;
-    }
-    return sum;
+Console.WriteLine();
+for (j = 0; j < n; j++)
+{
+randomArray[i,j] = rand.NextDouble();
+Console.Write($"{randomArray[i,j]:F2} ");
 }
+Console.WriteLine();
+}
+}
+mas(m,n);
+if (m2<1 || n2<1)
+Console.Write("Не могут быть отрицательными");
+else if (m2 <= m+1 && n2 <= n+1)
+Console.Write($"Значение элемента равно {randomArray[m2-1,n2-1]:F2} ");
+else Console.Write("Элемента нет в массиве");
