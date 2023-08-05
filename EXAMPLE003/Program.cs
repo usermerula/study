11eb96f1822ade05fa9223ba2e8e551a908f0820
usermerula <doublenope@mail.ru>
2023-08-06 01:19:28 +0300

    Задача 52

--- EXAMPLE003/Program.cs
+++ EXAMPLE003/Program.cs
@@ -1,7 +1,39 @@
-﻿int[] randomArray = new int[8];
+﻿Console.Write("Размер m массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Размер n массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] randomArray = new int[m,n];
 
-for (int i = 0; i < 8; i++)
+void mas(int m, int n)
 {
-randomArray[i] = new Random().Next(1,99);
-Console.Write(randomArray[i] + " " );
+int i,j;
+Random rand = new Random();
+for (i = 0; i < m; i++)
+{
+Console.WriteLine();
+for (j = 0; j < n; j++)
+{
+randomArray[i,j] = rand.Next(1,9);
+Console.Write($"{randomArray[i,j]} ");
 }
+Console.WriteLine();
+}
+}
+
+void arif(int m, int n)
+{
+Console.Write("Среднее арифметическое каждого столбца: ");
+int i,j;
+Random rand = new Random();
+for (j = 0; j < n; j++)
+{
+double sum = 0;
+for (i = 0; i < m; i++)
+{
+sum = sum + randomArray[i,j];
+}
+Console.Write($"{sum/(i):F1}; ");
+}
+}
+mas(m,n);
+arif(m,n);
