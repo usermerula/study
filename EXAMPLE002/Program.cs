d35498c78bf5b1e1803ba6ce802a22388ae5e6b1
usermerula <doublenope@mail.ru>
2023-08-14 13:24:56 +0300

    Задача 43

--- EXAMPLE002/Program.cs
+++ EXAMPLE002/Program.cs
@@ -1,31 +1,17 @@
-﻿Console.Write("Размер m массива: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Размер n массива: ");
-int n = Convert.ToInt32(Console.ReadLine());
-double[,] randomArray = new double[m,n];
-Console.Write("Позицию строки: ");
-int m2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Позицию столбца: ");
-int n2 = Convert.ToInt32(Console.ReadLine());
-
-void mas(int m, int n)
-{
-int i,j;
-Random rand = new Random();
-for (i = 0; i < m; i++)
-{
-Console.WriteLine();
-for (j = 0; j < n; j++)
-{
-randomArray[i,j] = rand.NextDouble();
-Console.Write($"{randomArray[i,j]:F2} ");
-}
-Console.WriteLine();
-}
-}
-mas(m,n);
-if (m2<1 || n2<1)
-Console.Write("Не могут быть отрицательными");
-else if (m2 <= m+1 && n2 <= n+1)
-Console.Write($"Значение элемента равно {randomArray[m2-1,n2-1]:F2} ");
-else Console.Write("Элемента нет в массиве");
+﻿Console.Write("Введите k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+ 
+ 
+var x = -(b1 - b2) / (k1 - k2);
+var y = k1 * x + b1;
+ 
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+ 
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
