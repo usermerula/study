0a8d076a37c77e3fc6ef3099367fff60d641ae50
usermerula <doublenope@mail.ru>
2023-08-14 13:45:00 +0300

    Задача 58

--- EXAMPLE003/Program.cs
+++ EXAMPLE003/Program.cs
@@ -1,39 +1,67 @@
-﻿Console.Write("Размер m массива: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Размер n массива: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int[,] randomArray = new int[m,n];
+﻿Console.WriteLine("Введите размеры матриц и диапазон случайных значений:");
+int m = InputNumbers("Введите число строк 1-й матрицы: ");
+int n = InputNumbers("Введите число столбцов 1-й матрицы (и строк 2-й): ");
+int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
+int range = InputNumbers("Введите диапазон случайных чисел: от 1 до ");
 
-void mas(int m, int n)
+int[,] firstMartrix = new int[m, n];
+CreateArray(firstMartrix);
+Console.WriteLine($"Первая матрица:");
+WriteArray(firstMartrix);
+
+int[,] secomdMartrix = new int[n, p];
+CreateArray(secomdMartrix);
+Console.WriteLine($"Вторая матрица:");
+WriteArray(secomdMartrix);
+
+int[,] resultMatrix = new int[m,p];
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"Произведение первой и второй матриц:");
+WriteArray(resultMatrix);
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
 {
-int i,j;
-Random rand = new Random();
-for (i = 0; i < m; i++)
-{
-Console.WriteLine();
-for (j = 0; j < n; j++)
-{
-randomArray[i,j] = rand.Next(1,9);
-Console.Write($"{randomArray[i,j]} ");
-}
-Console.WriteLine();
-}
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
 }
 
-void arif(int m, int n)
+int InputNumbers(string input)
 {
-Console.Write("Среднее арифметическое каждого столбца: ");
-int i,j;
-Random rand = new Random();
-for (j = 0; j < n; j++)
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
 {
-double sum = 0;
-for (i = 0; i < m; i++)
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArray (int[,] array)
 {
-sum = sum + randomArray[i,j];
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
 }
-Console.Write($"{sum/(i):F1}; ");
-}
-}
-mas(m,n);
-arif(m,n);
