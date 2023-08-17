2b781b878b349767573cab2124ab45ed39181070
usermerula <doublenope@mail.ru>
2023-08-18 00:45:02 +0300

    64

--- EXAMPLE001/Program.cs
+++ EXAMPLE001/Program.cs
@@ -1,52 +1,20 @@
-﻿int[,] table = new int[3, 4];
-FillArrayRandom(table);
-PrintArray(table);
-SortToLower(table);
-Console.WriteLine();
-PrintArray(table);
+﻿int n = InputNumbers("Введите N: ");
+int count = 2;
+PrintNumber(n, count);
+Console.Write(1);
 
-
-
-void FillArrayRandom(int[,] array)
+void PrintNumber(int n, int count)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().Next(1, 10);
-        }
-    }
+ if (count > n) return;
+ PrintNumber(n, count + 1);
+ Console.Write(count + ", ");
 }
 
-
-void SortToLower(int[,] array)
+int InputNumbers(string input)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            for (int k = 0; k < array.GetLength(1) - 1; k++)
-            {
-                if (array[i, k] < array[i, k + 1])
-                {
-                    int temp = array[i, k + 1];
-                    array[i, k + 1] = array[i, k];
-                    array[i, k] = temp;
-                }
-            }
-        }
-    }
+ Console.Write(input);
+ int output = Convert.ToInt32(Console.ReadLine());
+ return output;
 }
 
-
-void PrintArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write($"{array[i, j]} ");
-        }
-        Console.WriteLine();
-    }
-}
+  
